3dbadb8b2390cb6714153c47587d5ed54367d0c6
Xavier Carty <xaviercarty@gmail.com>
2020-03-30 03:04:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      search_term = req.params[item]
-      resp.write handle_search(search_term)
+        if cart.empty?
+          res.write ""
     else
       resp.write "Path Not Found"
     end
