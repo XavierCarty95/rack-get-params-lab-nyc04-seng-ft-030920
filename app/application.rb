0e66afd9d633d03058fc484d4106549588e5a223
Xavier Carty <xaviercarty@gmail.com>
2020-03-30 03:02:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -12,6 +13,9 @@ class Application
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      search_term = req.params[item]
       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
